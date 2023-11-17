4c51f1e40d6ec12f3bc6b15849fd0cc10569ce5e
RahulVuppu1a <vuppular1984@uhcl.edu>
2023-11-17 17:37:22 -0600

    Second VC created and components added

---
+++ Assignment4/Assignment4/View Controllers/DetailedVC.swift
@@ -0,0 +1,32 @@
+//
+//  DetailedVC.swift
+//  Assignment4
+//
+//  Created by rahul vuppula on 11/17/23.
+//
+
+import UIKit
+
+class DetailedVC: UIViewController {
+
+    @IBOutlet weak var descTxtField: UITextView!
+    @IBOutlet weak var imgview: UIImageView!
+    @IBOutlet weak var lblname: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
